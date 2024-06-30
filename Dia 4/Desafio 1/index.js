6612e217f3c9f8907885fccbbdb5984ac5280abd
Carlos Henrique Alves dos Santos <chads0rei10@gmail.com>
2024-06-30 08:24:32 -0300

    Organizando...

--- Dia 4/Desafio 1/index.js
+++
@@ -1,7 +0,0 @@
-let senteFome = Number(prompt('Você está com fome? Digite "1" para SIM, ou "2" para NÃO.'))
-let temDinheiro = Number(prompt('Você tem dinheiro? Digite "1" para SIM, ou "2" para NÃO.'))
-
-if(senteFome === 2 || temDinheiro === 2){console.log('Hoje a janta será em casa.')}
-else if(true){let restauranteAbertoOuFechado = Number(prompt('O restaurante que você quer ir está aberto? Digite "1" para SIM, ou "2" para NÃO.'))
-if(temDinheiro === 1 && restauranteAbertoOuFechado === 1){console.log('Hoje o jantar será no seu restaurante preferido!')}
-else{console.log('Peça um delivery!')}};
--- Dia 4/Desafio 2/index.js
+++
@@ -1,8 +0,0 @@
-let nome = String(prompt('Qual o seu nome?'))
-let idade = Number(prompt('Qual é a sua idade?'))
-let possuiCarteira = Number(prompt('Você possui carteira de motorista? Digite "1" para SIM, ou "2" para NÃO.'))
-let possuiCarro = Number(prompt('Você tem algum veículo? Digite "1" para SIM, ou "2" para NÃO.'))
-
-if(idade < 18 || possuiCarteira === 2){console.log(nome + ', você não pode dirigir.')}
-else if(possuiCarteira === 1 && possuiCarro === 1){console.log(nome + ', você será o motorista!')}
-else{console.log(nome + ', você pode dirigir, mas não tem um carro.')};
