6df0973e1dd80f9e359994ae89c42c66143e9c38
Carlos Henrique Alves dos Santos <chads0rei10@gmail.com>
2024-07-03 03:09:56 -0300

    Melhorando o código do Desafio 2 do Dia 5!

--- Dia 5/Desafio 2/index.js
+++ Dia 5/Desafio 2/index.js
@@ -1,20 +1,22 @@
 let valorDesejado = undefined
 let quantidadeEmLitros = undefined
 let OpçãoDoCliente = Number(prompt('Olá, seja bem vindo ao nosso posto de gasolina. ' 
-+ 'Por favor, digite " 1 " para abastecer com gasolina, " 2 " para abastecer com álcool, ou " 3 " para calibrar os pneus.'))
+    + 'Por favor, digite " 1 " para abastecer com gasolina, " 2 " para abastecer com álcool, ou " 3 " para calibrar os pneus.'))
 
 switch(OpçãoDoCliente){
 
     case 1:
         valorDesejado = Number(prompt('Por favor, digite o valor desejado em "R$" para ser abastecido com gasolina. (Valor do litro de gasolina: R$5.00)'))
-        quantidadeEmLitros = parseInt(valorDesejado / 5)
-        console.log('Muito bem! foram abastecidos ' + quantidadeEmLitros + ' litros de gasolina em seu veículo. Muito obrigado e volte sempre!')
+        if(isNaN(valorDesejado)){console.log('Ops, ouve algum erro. Por favor, tente novamente.')}
+        else{quantidadeEmLitros = parseInt(valorDesejado / 5)
+            console.log('Muito bem! foram abastecidos ' + quantidadeEmLitros + ' litros de gasolina em seu veículo. Muito obrigado e volte sempre!')}
         break;
 
     case 2:
         valorDesejado = Number(prompt('Por favor, digite o valor desejado em "R$" para ser abastecido com álcool. (Valor do litro de álcool: R$3.00)'))
-        quantidadeEmLitros = parseInt(valorDesejado / 3)
-        console.log('Muito bem! foram abastecidos ' + quantidadeEmLitros + ' litros de álcool em seu veículo. Muito obrigado e volte sempre!')
+        if(isNaN(valorDesejado)){console.log('Ops, ouve algum erro. Por favor, tente novamente.')}
+        else{quantidadeEmLitros = parseInt(valorDesejado / 3)
+            console.log('Muito bem! foram abastecidos ' + quantidadeEmLitros + ' litros de álcool em seu veículo. Muito obrigado e volte sempre!')}
         break;
 
     case 3:
