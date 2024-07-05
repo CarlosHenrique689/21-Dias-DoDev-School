32ba4c8556091aca6bc81c8a5ced7a81eb6b07b5
Carlos Henrique Alves dos Santos <chads0rei10@gmail.com>
2024-07-05 02:46:35 -0300

    Melhorando o código do Desafio 1 do Dia 4!

--- Dia 4/Desafio 1/index.js
+++ Dia 4/Desafio 1/index.js
@@ -1,7 +1,14 @@
 let senteFome = Number(prompt('Você está com fome? Digite "1" para SIM, ou "2" para NÃO.'))
 let temDinheiro = Number(prompt('Você tem dinheiro? Digite "1" para SIM, ou "2" para NÃO.'))
+let restauranteAbertoOuFechado = undefined
 
-if(senteFome === 2 || temDinheiro === 2){console.log('Hoje a janta será em casa.')}
-else if(true){let restauranteAbertoOuFechado = Number(prompt('O restaurante que você quer ir está aberto? Digite "1" para SIM, ou "2" para NÃO.'))
-if(temDinheiro === 1 && restauranteAbertoOuFechado === 1){console.log('Hoje o jantar será no seu restaurante preferido!')}
-else{console.log('Peça um delivery!')}};
+if(senteFome === 1 || senteFome === 2)
+    {if(temDinheiro === 1 || temDinheiro === 2)
+        {if(senteFome === 2 || temDinheiro === 2){console.log('Hoje a janta será em casa.')}
+        else if(true){restauranteAbertoOuFechado = Number(prompt('O restaurante que você quer ir está aberto? Digite "1" para SIM, ou "2" para NÃO.'))
+            if(restauranteAbertoOuFechado === 1 || restauranteAbertoOuFechado === 2)
+                {if(temDinheiro === 1 && restauranteAbertoOuFechado === 1){console.log('Hoje o jantar será no seu restaurante preferido!')}
+                else{console.log('Peça um delivery!')}}
+            else{console.log('Erro... Por favor, tente novamente usando "1" para SIM, ou "2" para NÃO.')}}}            
+    else{console.log('Erro... Por favor, tente novamente usando "1" para SIM, ou "2" para NÃO.')}}
+else{console.log('Erro... Por favor, tente novamente usando "1" para SIM, ou "2" para NÃO.')};
