73fb8f31d92ba908319f04a2722019d0d533c590
Carlos Henrique Alves dos Santos <chads0rei10@gmail.com>
2025-03-24 13:44:30 -0300

    Enviando o desafio com todos os bugs resolvidos!

--- Dia 05/Desafio Caça Bugs Condicional/index.js
+++ Dia 05/Desafio Caça Bugs Condicional/index.js
@@ -1,20 +1,30 @@
 //CAÇA BUGS - CONDICIONAIS
 
-let iniciar = prompt("Deseja iniciar o programa? s/n")
+let iniciar = prompt("Deseja iniciar o programa? s/n");
 
-if (iniciar == "s") {
+if (iniciar === "n") {
     alert("Programa encerrado!")
-} else {
+} else if (iniciar === "s") {
     alert("Programa iniciado!")
-    let opcao = prompt("Escolha uma ação a ser feita -> 1 - Mensagem motivacional; 2 - Mensagem desmotivacional; 3 - nada")
-    switch (iniciar) {
+    let opcao = Number(prompt("Escolha uma ação a ser feita -> 1 - Mensagem motivacional; 2 - Mensagem desmotivacional; 3 - nada"))
+    switch (opcao) {
         case 1:
-            alert("Você NÃO VAI conseguir, desista!")
-        case 2:
             alert("Continue tentando, você VAI conseguir!")
+            alert("Programa encerrado!")
+            break;
+        case 2:
+            alert("Você NÃO VAI conseguir, desista!")
+            alert("Programa encerrado!")
+            break;
         case 3:
-            alert("Opção inválida selecione um número de 1 a 3.")
-        default:
             alert("...")
+            alert("Programa encerrado!")
+            break;
+        default:
+            alert("Opção inválida, reinicie o programa e selecione um número de 1 a 3.")
+            alert("Programa encerrado!")
+            break;
     }
-}
+} else {alert("Opção inválida, reinicie o programa e selecione uma opção válida")
+        alert("Programa encerrado!")
+};
